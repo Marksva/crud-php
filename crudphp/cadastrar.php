50f4e8e29e266bd5242ee2ed8871d56735a8c9c6
Mark Anthony <89271406+Marksva@users.noreply.github.com>
2022-06-30 22:36:12 -0300

    up

---
+++ crudphp/cadastrar.php
@@ -0,0 +1,13 @@
+<h1>Cadastrar usuario</h1>
+
+<form method="POST" action="cadastrar_action.php">
+
+    <label>
+        Nome: <input type="text" name="nome" require />
+    </label>
+    <label>
+        Email: <input type="email" name="email" require />
+    </label>
+    <input type="submit" value="Cadastrar" />
+
+</form>
